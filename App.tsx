22dc7f332306ed9c941aee7c37fbe9cbe8978e30
HoangPhuc2709 <phuctran27092004@gmail.com>
2025-11-21 20:12:15 +0700

    build

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Services from './components/Services';
-import AIConsultant from './components/AIConsultant';
+// import AIConsultant from './components/AIConsultant';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import AdminLogin from './components/AdminLogin';
@@ -45,7 +45,7 @@ const AppContent: React.FC = () => {
         <About />
         <Projects />
         <Services />
-        <AIConsultant />
+        {/* <AIConsultant /> */}
         <Contact />
       </main>
       <Footer />
